3ed15135248c5f69cdf7e05751701172f97a1a6e
Thasi Mhlongo <bruce.mhlongo21@gmail.com>
2019-08-06 22:40:55 +0200

    Update project

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://thasi:thasi0708@ds121301.mlab.com:21301/mern_shoppingzar'
+};
---
+++ server.js
@@ -0,0 +1,26 @@
+const express       = require('express');
+const mongoose      = require('mongoose');
+const bodyParser    = require('body-parser');
+
+const items = require("./routes/api/items");
+
+const app = express();
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to Mongo
+mongoose
+.connect(db)
+.then(() => console.log("MongoDB Connected..."))
+.catch(err => console.log(err));
+
+// Use Routes
+app.use('/api/items', items);
+
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("SINYILE, SERVER HAS STARTED!!!"); 
+});
